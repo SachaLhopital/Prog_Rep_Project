33b25d83df2b15cacf7ff6d53779b7ee2142358c
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2017-05-15 16:37:45 +0200

    Fix installation problems

--- permispiste/src/main/java/com/epul/PermispisteApplication.java
+++ permispiste/src/main/java/com/epul/PermispisteApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class PermispisteApplication{
 
+	//Todo : remove
+
 	public static void main(String[] args) {
 		SpringApplication.run(PermispisteApplication.class, args);
 	}
